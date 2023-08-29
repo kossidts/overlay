631ff3d7744df41d8cba5da23f9c49b69ff6161c
Kossi D. T. Saka <kdtsaka@gmail.com>
2023-08-30 00:40:55 +0200

    New Vite + Typescript project to create a generic overlay component

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <div class="card">
+      <button id="modaltoaster" type="button">Open the modal</button>
+    </div>
+  </div>
+`;
